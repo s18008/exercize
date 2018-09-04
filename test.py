f8bb93d3ed070736f9e9305d23fa4671f43239f7
s18008 <s18008@std.it-college.ac.jp>
2018-09-04 13:47:23 +0900

    課題

--- test.py
+++ test.py
@@ -1,32 +1,37 @@
 import tkinter,random
 quiz = [('1+1=','1','2','3','4',2),('2+2=','4','6','8','10',1)]
-Q = random.sample(quiz,len(quiz))
+q = random.sample(quiz,len(quiz))
 Next = 0
 
 root = tkinter.Tk()
 root.title(u"Software Title")
 root.geometry("400x300")
 
-def nest(number):
+def nest1(number):
     def Decision():
-        global Next,Static
-        if number == Q[Next][5]:
-           ## Static = tkinter.Label(text='正解')
+        global Next
+        if number == q[Next][5]:
             Static.config(text="正解")
-            ##Static.pack()
         else:
-            ##Static = tkinter.Label(text='不正解')
-            ##Static.pack()
             Static.config(text="不正解")
-        sel = tkinter.Button(text="次の問題へ",width=50,command=lambda: sel.pack_forget())
-        sel.pack()
+        Next += 1
+        start.config(text="次の問題へ",width=50,command=nest2())
     return Decision
 
-Static = tkinter.Label(text=Q[Next][0])
-Static.pack()
-for i in range(1,5):
-    Button = tkinter.Button(text="1:"+Q[Next][i],width=50,command=nest(Q[Next].index(Q[Next][i])))
-    Button.pack()
+def nest2():
+    def question():
+        Static.config(text=q[Next][0])
+        for i in range(1,5):
+           Button = tkinter.Button(text="1:"+q[Next][i],width=50,command=nest1(q[Next].index(q[Next][i])))
+           Button.pack()
+    return question
 
+Static = tkinter.Label(text='')
+Static.pack()
+start = tkinter.Button(text="問題を始める",width=50,command=nest2())
+start.pack()
+#for i in Choices:
+#    i = tkinter.Button(text="")
+#    i.pack()
 root.mainloop()
 
